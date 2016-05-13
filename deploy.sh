05d87b39bdcc45782f7ac7a9a7c8f686e16298bb
Jason.Zhang <lefttjs@gmail.com>
2016-05-13 12:53:12 +0800

    change port at production

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-export NODE_ENV=production
+export NODE_ENV=production PORT=7777
 git reset --hard
 git pull origin HEAD
 npm install
 gulp yaml
 pm2 stop warm-mock -f
-pm2 start ./app.js -n warm-mock
+pm2 start ./bin/www -n warm-mock
